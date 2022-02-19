8765c23a78e0e4af7070d20d8893ddcba4c886ee
Chukwuka Morka <chukwukamorka@gmail.com>
2022-02-19 01:30:00 +0100

    Added Request Filter & Vue Persisted State Plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 import coachesModule from './modules/coaches/index';
 import requestsModule from './modules/requests/index';
 
 const store = createStore({
+  plugins: [createPersistedState()],
   modules: {
     coachesModule, requestsModule
   },
--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,6 +1,10 @@
 const getters = {
-    requests: (state) => state.requests,
-    hasRequests: (state) => state.requests && state.requests.length > 0,
+    requests: (state, _, _2, rootGetters) => {
+        const coachId = rootGetters.userId;
+        return state.requests.filter(request => request.coachId === coachId);
+    },
+    hasRequests: (_, getters) => getters.requests && getters.requests.length > 0,
+
 }
 
 export default getters;
